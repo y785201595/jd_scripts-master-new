4ff1f076ec2a287d5d6038acc68cd99c3419c9ac
y785201595 <785201595@qq.com>
2022-05-16 13:33:46 +0800

    202205161333

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,7 +10,7 @@
 0 3,13 * * * node /scripts/jd_xmf.js >> /scripts/logs/jd_xmf.log 2>&1
 
 #天天压岁钱
-0 10,13 * * * node /scripts/jd_xiaolongfan.js >> /scripts/logs/jd_xiaolongfan.js.log 2>&1
+30 6,10,14,18,20 * * * node /scripts/jd_ddq.js >> /scripts/logs/jd_ddq.log 2>&1
 
 #jd_nhs_rpb.js
 0 0,23 * * * node /scripts/jd_nhs_rpc.js >> /scripts/logs/jd_nhs_rpc.log 2>&1
